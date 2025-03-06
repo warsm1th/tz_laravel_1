74d8eec96a15391f710bd994e4a506d1037d5caf
Гасанов Курбан Гасанович <k.gasanov@aaagroup.ru>
2025-03-06 22:53:31 +0300

    создание моделей, миграций и контроллеров

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\OrderController;
+
+
+Route::get('/', [HomeController::class, 'index']);
+Route::resource('categories', CategoryController::class);
+Route::resource('products', ProductController::class);
+Route::resource('orders', OrderController::class);
